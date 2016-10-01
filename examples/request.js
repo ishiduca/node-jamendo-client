cb20c2a35473191ab858c1fb49b1004e30b5b877
ishiduca <ishiduca@gmail.com>
2016-10-01 18:02:19 +0900

    v0.0.1 create jamendo.js, example/request.js

--- examples/request.js
+++ examples/request.js
@@ -1,18 +1,10 @@
-# jamendo-client
-
-a thin http client for Jamendo API v3.0
-
-## usage
-
-to download "mp3" in order.
-
-```js
 'use strict'
-var fs        = require('fs')
-var path      = require('path')
-var mkdirp    = require('mkdirp')
-var through   = require('through2')
-var jamendo   = require('../jamendo')(your_client_id, {
+const fs        = require('fs')
+const path      = require('path')
+const mkdirp    = require('mkdirp')
+const through   = require('through2')
+const client_id = require('../test_client_id').client_id
+const jamendo   = require('../jamendo')(client_id, {
     https: true
   , followRedirects: true
 })
@@ -21,22 +13,20 @@ jamendo.request('/tracks', {
     artist_name: 'Golden Duck Orchestra'
   , order: 'name'
 })
-    .on('error', err => console.error(err))
-    .once('Jamendo.Api.Response', ret => console.dir(ret.headers))
-
+.on('error', err => console.error(err))
+.once('Jamendo.Api.Response', ret => console.dir(ret.headers))
 .pipe(through.obj((result, _, done) => {
-    var dirPath = path.join(__dirname, result.artist_name, result.album_name)
+    const dirPath = path.join(__dirname, result.artist_name, result.album_name)
     mkdirp(dirPath, err => {
         if (err) return onError(err)
 
-        var filePath = path.join(dirPath, result.name) + '.mp3'
+        const filePath = path.join(dirPath, result.name) + '.mp3'
 
         console.log('[download start "%s"]', result.name)
 
         jamendo.request('/tracks/file', {id: result.id})
             .on('error', onError)
-
-        .pipe( fs.createWriteStream(filePath))
+            .pipe(fs.createWriteStream(filePath))
             .on('error', onError)
             .once('finish', () => {
                 console.log('[download ok   "%s"]', result.name)
@@ -48,5 +38,4 @@ jamendo.request('/tracks', {
         console.error(err)
         done()
     }
-})
-```
+}))
---
+++ jamendo.js
@@ -0,0 +1,204 @@
+var url        = require('url')
+var xtend      = require('xtend')
+var bl         = require('bl')
+var hyperquest = require('hyperquest')
+var stream     = require('readable-stream')
+
+var HOSTNAME = 'api.jamendo.com'
+var VERSION  = 'v3.0'
+
+module.exports = exports = Jamendo
+
+if (process.env.DEV === 'test') Jamendo._createRequestURI = createRequestURI
+
+function Jamendo (client_id, _client_secret, _opt) {
+    if (!(this instanceof Jamendo)) return new Jamendo(client_id, _client_secret, _opt)
+
+    if (! client_id) throw new Error('"client_id" not found')
+    if (typeof client_id !== 'string') throw new TypeError('"client_id" must be "string"')
+
+    if (_client_secret && isObject(_client_secret)) {
+        _opt            = _client_secret
+        _client_secret  = null
+    }
+
+    if (_client_secret) {
+        if (typeof _client_secret !== 'string') throw new TypeError('"_client_secret" must be "string"')
+        this.client_secret = _client_secret
+    }
+
+    var opt = xtend(_opt)
+    this.client_id = client_id
+    this.protocol  = opt.https ? 'https:' : 'http:'
+    this.hostname  = HOSTNAME
+    this.version   = VERSION
+    this.followRedirects = opt.followRedirects
+}
+
+Jamendo.prototype.request = function (endpoint, _params, _opt, cb) {
+    if (! endpoint) throw new Error('"endpoint" not found')
+    if (typeof endpoint !== 'string') throw new TypeError('"endpoint" must be "string"')
+    if (! _params) throw new Error('"_params" not found')
+    if (! isObject(_params)) throw new TypeError('"_params" must be "object"')
+    if (typeof _opt === 'function') {
+        cb = _opt
+        _opt = {}
+    }
+    if (typeof cb !== 'function') cb = null
+
+    var opt    = xtend(_opt)
+    var params = xtend(_params, {client_id: this.client_id})
+    var uri    = createRequestURI(endpoint, params, this.protocol, this.hostname, this.version)
+
+    if (/\/file\/?$/.test(endpoint))
+        return file(uri, opt, cb)
+    else
+        return get(uri, opt, cb, this.followRedirects)
+}
+
+function file (uri, opt, cb) {
+    var ws = new stream.Transform
+    ws._transform = function (chnk, _, done) {
+        done(null, chnk)
+    }
+
+    handle(uri, opt)
+
+    return ws
+
+    function onError (err) {
+        cb ? cb(err) : ws.emit('error', err)
+    }
+
+    function handle (uri, opt) {
+        var hyp = hyperquest(uri, opt)
+
+        hyp.on('response', function (response) {
+            if (isRedirect(hyp.request, response)) {
+                return handle(url.resolve(uri, response.headers.location), opt)
+            }
+
+            if (response.statusCode >= 400 && response.statusCode < 600) {
+                return response.pipe(bl(function (err, data) {
+                    if (! err) {
+                        err = new Error(String(data))
+                        err.statusCode = response.statusCode
+                    }
+                    onError(err)
+                }))
+            }
+
+            response.pipe(ws)
+
+            if (cb) {
+                response.pipe(bl(function (err, data) {
+                    cb(err, data, response)
+                }))
+            }
+        })
+
+        return hyp
+    }
+}
+
+function get (uri, opt, cb, _followRedirects) {
+    var followRedirects = !! _followRedirects
+    var redirect        = null
+    var count           = followRedirects ? 0 : null
+    var maxRedirects    = followRedirects ? typeof _followRedirects === 'number' ? parseInt(_followRedirects, 10) : 10 : 0
+
+    opt.headers = xtend(opt.headers, {'accept': 'application/json'})
+
+    var rs = new stream.Readable({objectMode: true})
+    rs._read = function () {}
+
+    handle(uri, opt)
+
+    return rs
+
+    function onError (err) {
+        cb ? cb(err) : rs.emit('error', err)
+    }
+
+    function handle (uri, opt) {
+        var hyp = hyperquest(uri, opt)
+
+        if (followRedirects) {
+            hyp.on('response', function (response) {
+                isRedirect(hyp.request, response) && (redirect = response.headers.location)
+            })
+        }
+
+        hyp.pipe(bl(function (err, data) {
+            if (redirect) {
+                if ((count += 1) >= maxRedirects)
+                    return onError(new Error('Response was redirected too many times :('))
+
+                var headers = xtend(opt.headers, {referer: uri})
+                handle(url.resolve(uri, redirect), xtend(opt, {headers: headers}))
+                return (redirect = null)
+            }
+
+            rs.emit('response', hyp.response)
+
+            if (err) return onError(err)
+
+            if (! data.length) return cb(null, null, hyp.response)
+
+            var ret
+            try {
+                ret = JSON.parse(String(data))
+            } catch (_err) {
+                var e = new SyntaxError('JSON parse error: ' + _err.message, _err)
+                e.data = data
+                e.response = hyp.response
+                return onError(e)
+            }
+
+            rs.emit('Jamendo.Api.Response', ret)
+
+            if (ret.headers && ret.headers.code !== 0) {
+                var e = new Error(ret.headers.error_message)
+                e.name = ret.headers.status
+                e.data = ret.headers
+                return onError(e)
+            }
+
+            ;[].concat(ret.results).forEach(function (result) {
+                rs.push(result)
+            })
+            rs.push(null)
+            cb && cb(null, ret, hyp.response)
+        }))
+
+        return hyp
+    }
+}
+
+function createRequestURI (endpoint, params, protocol, host, version) {
+    if (endpoint.slice(0, 1) !== '/') endpoint = '/' + endpoint
+    if (endpoint.slice(-1)   !== '/') endpoint = endpoint + '/'
+
+    var querys = []
+    for (var p in params) {
+        if (Object.prototype.hasOwnProperty.apply(params, [p]))
+            querys = querys.concat([encodeURIComponent(p), map(params[p])].join('='))
+    }
+
+    return protocol + '//' + host + '/' + version + endpoint + '?' + querys.join('&')
+
+    function map (p) {
+        if (! Array.isArray(p)) p = String(p).split(' ')
+        return p.map(encodeURIComponent).join('+')
+    }   
+}
+
+function isRedirect (req, res) {
+    return req.method === 'GET' &&
+           res.headers.location &&
+           String(res.statusCode).slice(0, 1) === '3'
+}
+
+function isObject (p) {
+    return Object.prototype.toString.apply(p) === '[object Object]'
+}
